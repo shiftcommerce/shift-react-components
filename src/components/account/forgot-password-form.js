ea550fe002a2953f0f6292c7979922a29947ac51
Owen Smith <ozz1649@gmail.com>
2019-03-22 10:06:03 +0000

    created 2 forms for resetting password, styled them

---
+++ src/components/account/forgot-password-form.js
@@ -0,0 +1,83 @@
+// Libraries
+import React, { Component } from 'react'
+import classNames from 'classnames'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+
+// lib
+import componentMapping from '../../lib/component-mapping'
+
+class ForgotPasswordForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.Button = componentMapping('Button')
+  }
+
+  renderSubmitButton() {
+    return (
+      <this.Button
+        className='c-password__button o-button-sml'
+        aria-label='Submit'
+        label='SUBMIT'
+        status='primary'
+        type='submit'
+      />
+    )
+  }
+
+  renderFlashMessage () {
+    return (
+      <div className='c-password__flash-message'>
+        <a>Password reset email sent.</a>
+        <a>Please allow up to 30 mins for the email to arrive.</a>
+      </div>
+    )
+  }
+
+  render() {
+    const {
+      className,
+      handleSubmit,
+      flashMessage
+    } = this.props
+
+    console.log(flashMessage)
+
+    const initialValues = {
+      email: ''
+    }
+
+    const emailSchema = Yup.object().shape({
+      email: Yup.string()
+        .email('Invalid email')
+        .required('Email is required')
+    })
+
+    return (
+      <div className={classNames('o-form', className)}>
+        {flashMessage && this.renderFlashMessage()}
+        <div className='c-password'>
+          <h1 className='c-password__title'>Forgot Password</h1>
+          <p className='c-password__caption'>Please enter your email address and submit. In doing this an email containing a special link will be mailed to you. Once received, click on this link and you will then have the opportunity to enter a new password.</p>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={emailSchema}
+            submitForm={handleSubmit}
+            render={({ errors, status, touched, isSubmitting }) => (
+              <Form>
+                <Field type='email' name='email' placeholder='Email' className='o-form__input-field o-form__input-block' />
+                <div className='o-form__input-field__error'>
+                  <ErrorMessage name='email' />
+                </div>
+                { this.renderSubmitButton() }
+              </Form>
+            )}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ForgotPasswordForm
---
+++ src/components/account/password-reset-form.js
@@ -0,0 +1,72 @@
+// Libraries
+import React, { Component } from 'react'
+import classNames from 'classnames'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+
+// lib
+import componentMapping from '../../lib/component-mapping'
+
+class PasswordResetForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.Button = componentMapping('Button')
+    this.AccountFormErrors = componentMapping('FormErrors')
+  }
+
+  renderSubmitButton() {
+    return (
+      <this.Button
+        className='c-password__button o-button-sml'
+        aria-label='Submit'
+        label='submit'
+        status='primary'
+        type='submit'
+      />
+    )
+  }
+
+  render() {
+    const {
+      className,
+      handleSubmit
+    } = this.props
+
+    const initialValues = {
+      password: ''
+    }
+
+    const passwordSchema = Yup.object().shape({
+      password: Yup.string()
+        .min(5, 'Password must be at least 5 characters')
+        .max(30, 'Password must be shorter than 30 characters')
+        .required('Required'),
+    })
+
+    return (
+      <div className={classNames('o-form', className)}>
+        <div className='c-password'>
+          <h1 className='c-password__title'>Password Reset</h1>
+          <p className='c-password__caption'>Please enter your new password</p>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={passwordSchema}
+            submitForm={handleSubmit}
+            render={({ errors, status, touched, isSubmitting }) => (
+              <Form>
+                <Field type='password' name='password' placeholder='New Password' className='o-form__input-field o-form__input-block' />
+                <div className='o-form__input-field__error'>
+                  <ErrorMessage name='password' />
+                </div>
+                { this.renderSubmitButton() }
+              </Form>
+            )}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PasswordResetForm
