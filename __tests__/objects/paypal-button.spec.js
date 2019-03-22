57a846a2742c821615f42b56f0c55d529eb98fd0
Mufudzi Masaire <mufudzi.masaire@shiftcommerce.com>
2019-03-22 11:46:05 +0000

    Basic PayPalButton object spec

---
+++ __tests__/objects/paypal-button.spec.js
@@ -0,0 +1,31 @@
+// Libraries
+import React from 'react'
+
+// Object
+import PayPalButton from '../../src/objects/paypal-button'
+
+test('renders correctly', () => {
+  // Arrange
+  global.paypal = {
+    Buttons: {
+      driver: jest.fn(() => {
+        return jest.fn()
+      })
+    }
+  } // set mock window.paypal
+  const paypalCreateOrder = jest.fn()
+  const paypalOnApprove = jest.fn()
+  const onClick = jest.fn()
+
+  // Act
+  const wrapper = shallow(
+    <PayPalButton 
+      paypalCreateOrder={paypalCreateOrder}
+      paypalOnApprove={paypalOnApprove}
+      onClick={ onClick }
+    />
+  )
+
+  // Assert
+  expect(wrapper).toMatchSnapshot()
+})
