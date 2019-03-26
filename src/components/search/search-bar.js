5df0c8c7b38c2d339d45d77d02044c71d8953142
Hubert Pompecki <hpompecki@gmail.com>
2019-03-26 11:15:17 +0000

    Rewrite search box using a connector

--- src/components/search/search-bar.js
+++ src/components/search/search-bar.js
@@ -1,17 +1,39 @@
 // Libraries
 import React, { Component } from 'react'
-import { SearchBox } from 'react-instantsearch/dom'
+import { connectSearchBox } from 'react-instantsearch-dom'
 
 export class SearchBar extends Component {
   render () {
+    const { currentRefinement, refine } = this.props
+
     return (
       <div className='c-searchbar'>
         <div className='c-searchbar__content'>
-          <SearchBox />
+          <div className='c-searchbox'>
+            <form
+              action=''
+              className='c-searchbox__form'
+              noValidate
+              onSubmit={e => e.preventDefault()}
+              role='search'
+            >
+              <input
+                className='c-searchbox__input'
+                onChange={event => refine(event.currentTarget.value)}
+                type='search'
+                value={currentRefinement}
+              />
+              <button
+                className='c-searchbox__submit'
+                title='Submit your search query.'
+                type='submit'
+              />
+            </form>
+          </div>
         </div>
       </div>
     )
   }
 }
 
-export default SearchBar
+export default connectSearchBox(SearchBar)
