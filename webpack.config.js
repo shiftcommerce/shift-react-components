512c7bf795071967f4bd0c72c2b1937ec116bb70
Owen Smith <ozz1649@gmail.com>
2019-04-11 09:30:30 +0100

    Update webpack.config.js
    Co-Authored-By: hubertpompecki <hpompecki@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var webpack = require('webpack')
+const webpack = require('webpack')
 const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
