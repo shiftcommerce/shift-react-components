86e099d0542cbac3473f77fe2b66d7815e31e5fd
Arron Fletcher <arron.fletcher@shiftcommerce.com>
2019-05-20 15:19:29 +0100

    bumpped version to 0.12.8

--- webpack.config.js
+++ webpack.config.js
@@ -74,7 +74,7 @@ module.exports = {
       { from: './src/scss/', to: 'scss/' }
     ]),
     new BundleAnalyzerPlugin({
-      analyzerMode: 'server'
+      analyzerMode: process.env.ANALYZE_BUNDLE ? 'server' : 'disabled'
     }),
     new DuplicatePackageCheckerPlugin(),
     new ExtractTextPlugin('bundle.css'),
